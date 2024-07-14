04d21a2b52628937a27cad0acd18847f476cd165
Nitish Nashine <nitishnashine@gmail.com>
2024-07-14 22:57:50 +0530

    done sortColors

--- sortColors.java
+++ sortColors.java
@@ -1,5 +1,23 @@
 public class sortColors {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public void sortColor(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int counter = 0;
+        while (counter <= end) {
+            if (nums[counter] == 0) {
+                int temp = nums[start];
+                nums[start] = nums[counter];
+                nums[counter] = temp;
+                start++;
+                counter++;
+            } else if (nums[counter] == 2) {
+                int temp = nums[end];
+                nums[end] = nums[counter];
+                nums[counter] = temp;
+                end--;
+            } else {
+                counter++;
+            }
+        }
     }
 }
